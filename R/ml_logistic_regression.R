d1764df91f4805f4cefa9b34cbc0fca4af09dcf4
catlain <catlain@vip.qq.com>
2017-05-12 02:46:57 +0000

    add "standardization","weightcol","threshold" to lr

---
+++ R/ml_logistic_regression.R
@@ -0,0 +1,70 @@
+ml_logistic_regression <- function (x, response, features, intercept = TRUE, standardization = TRUE, weightcol = NULL, threshold = 0.5, alpha = 0,
+          lambda = 0, iter.max = 100L, ml.options = ml_options(),
+          ...)
+{
+  ml_backwards_compatibility_api()
+  df <- spark_dataframe(x)
+  sc <- spark_connection(df)
+  categorical.transformations <- new.env(parent = emptyenv())
+  df <- ml_prepare_response_features_intercept(x = df, response = response,
+                                               features = features, intercept = intercept, envir = environment(),
+                                               categorical.transformations = categorical.transformations,
+                                               ml.options = ml.options)
+  alpha <- ensure_scalar_double(alpha)
+  lambda <- ensure_scalar_double(lambda)
+  iter.max <- ensure_scalar_integer(iter.max)
+  only.model <- ensure_scalar_boolean(ml.options$only.model)
+  standardization <- ensure_scalar_boolean(standardization)
+
+  weightcol <- ifelse(is.null(weightcol), NULL, ensure_scalar_character(weightcol))
+  threshold <- ifelse(is.list(threshold), lapply(threshold, function(x) ensure_scalar_integer(x)), ensure_scalar_integer(threshold))
+
+  envir <- new.env(parent = emptyenv())
+  envir$id <- ml.options$id.column
+  df <- df %>% sdf_with_unique_id(envir$id) %>% spark_dataframe()
+  tdf <- ml_prepare_dataframe(df, features, response, ml.options = ml.options,
+                              envir = envir)
+  envir$model <- "org.apache.spark.ml.classification.LogisticRegression"
+  lr <- invoke_new(sc, envir$model)
+  model <- lr %>%
+    invoke("setMaxIter", iter.max) %>%
+    invoke("setFeaturesCol",envir$features) %>%
+    invoke("setLabelCol", envir$response) %>%
+    invoke("setFitIntercept", as.logical(intercept)) %>%
+    invoke("setElasticNetParam", as.double(alpha)) %>%
+    invoke("setStandardization", as.logical(standardization))
+
+  if(!is.null(weightcol)){
+    model <- lr %>%
+      invoke("setWeightCol", weightcol)
+  }
+
+  if(is.list(threshold)){
+    model <- lr %>%
+    invoke("setThresholds", threshold)
+  }else{
+    model <- lr %>%
+    invoke("setThreshold", threshold)
+  }
+
+  if (only.model)
+    return(model)
+  fit <- model %>% invoke("fit", tdf)
+  coefficients <- fit %>% invoke("coefficients") %>% invoke("toArray")
+  names(coefficients) <- features
+  hasIntercept <- invoke(fit, "getFitIntercept")
+  if (hasIntercept) {
+    intercept <- invoke(fit, "intercept")
+    coefficients <- c(coefficients, intercept)
+    names(coefficients) <- c(features, "(Intercept)")
+  }
+  summary <- invoke(fit, "summary")
+  areaUnderROC <- invoke(summary, "areaUnderROC")
+  roc <- sdf_collect(invoke(summary, "roc"))
+  coefficients <- intercept_first(coefficients)
+  ml_model("logistic_regression", fit, features = features,
+           response = response, intercept = intercept, coefficients = coefficients,
+           roc = roc, area.under.roc = areaUnderROC, data = df,
+           ml.options = ml.options, categorical.transformations = categorical.transformations,
+           model.parameters = as.list(envir))
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+$trim_whitespace
+[1] "trim_whitespace"
+
+$sdf_repartition
+[1] "sdf_repartition"
+
+$ft_word2Vec
+[1] "ft_word2Vec"
+
+$spark_read_orc
+[1] "spark_read_orc"
+
+$ft_TFIDF
+[1] "ft_TFIDF"
+
+$sdf_pivot
+[1] "sdf_pivot"
+
+$spark_read_text
+[1] "spark_read_text"
+
+$sdf_showDF
+[1] "sdf_showDF"
+
+$sdf_partitions_size
+[1] "sdf_partitions_size"
+
+$sdf_schema_tree
+[1] "sdf_schema_tree"

4838753541376ce22c928feb443043e175deee61
catlain <catlain@vip.qq.com>
2017-05-16 19:28:10 +0800

    fixed

--- R/hello.R
+++ R/hello.R
@@ -64,7 +64,7 @@
 #
 # hostapk <- list("dqdqdad", "adqwdqd", "dqdqd")
 
-ft_cfrank <- function(sc, aid_vec, pkg_vec, aidvec_col = "aidarrayrun_vec", pkgvec_col = "runapp_vec", aid_col = "aid", target_pkg = "com.cmcm.live") {
+ft_cfrank <- function(sc, aid_vec, pkg_vec, aidvec_col = "runapp_vec", pkgvec_col = "aidarrayrun_vec", aid_col = "aid", target_pkg = "com.cmcm.live") {
   sdf_aid_vec <- spark_dataframe(aid_vec)
   sdf_pkg_vec <- spark_dataframe(pkg_vec)
   sdf <- sparklyr::invoke_static(sc, "Sparklyrfun.CF", "getRank", sdf_aid_vec, sdf_pkg_vec, aidvec_col, pkgvec_col, aid_col, target_pkg)
--- java/hello.scala
+++ java/hello.scala
@@ -11,21 +11,19 @@ object CF {
     * @param pkgVec
     * @param tarpkg
     */
-  def getRank(aidVec:DataFrame, pkgVec:DataFrame, aidVecCol:String = "aidarrayrun_vec", pkgVecCol:String = "runapp_vec", aidCol:String = "aid", tarpkg:String = "com.cmcm.live") = {
-    val pkgVec1 = pkgVec.filter(col(pkgVecCol) === tarpkg).collect()(0).
+  def getRank(aidVec:DataFrame, pkgVec:DataFrame, aidVecCol:String = "runapp_vec", pkgVecCol:String = "aidarrayrun_vec", aidCol:String = "aid", tarpkg:String = "com.cmcm.live") = {
+    
+    val pkgVecTarget = pkgVec.filter(col(pkgVecCol) === tarpkg).collect()(0).
       getAs[SparseVector](pkgVecCol).toArray
 
-    val aidVec1 = aidVec.collect()(0).getAs[SparseVector](aidVecCol)
-
-
     val udf1 = udf((runapp_vec: SparseVector, pkgVecArr:Seq[Double]) =>{
-      val elemWiseProd: Array[Double] = runapp_vec.toArray.zip(pkgVecArr.toArray[Double]).map(entryTuple => entryTuple._1 * entryTuple._2)
+      val elemWiseProd: Array[Double] = runapp_vec.toArray.zip(pkgVecArr.toArray[Double]).map(entryTuple => entryTuple._1 * entryTuple._2) //udf case array => wrapped array
       elemWiseProd.sum
     })
     
     aidVec.select(col(aidCol), col(aidVecCol)).
-    withColumn("pkgVecArr",lit(pkgVec1)).
-    select(udf1(col(pkgVecCol), col("pkgVecArr")).alias("rank")) //udf case array => wrapped array
+    withColumn("pkgVecArr",lit(pkgVecTarget)).
+    select(udf1(col(pkgVecCol), col("pkgVecArr")).alias("rank")) 
   }
 }
 

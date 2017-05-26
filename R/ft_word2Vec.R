0eaef76bc88935b862aadb57f93035d34c220a06
catlain <catlain@vip.qq.com>
2017-05-26 22:10:57 +0800

    ft_word2Vec:add getvectors

--- R/ft_word2Vec.R
+++ R/ft_word2Vec.R
@@ -1,14 +1,31 @@
-ft_word2Vec <- function (x, input.col = NULL, output.col = NULL, vector.size = 10L, min.count = 0L, num.partitions = 100L, step.size = 0.025, max.iter = 1000, window.size = 1L, ...) {
+ft_word2Vec <- function (x, input.col = NULL, output.col = NULL, vector.size = 10, min.count = 0L, num.partitions = 100,
+                         step.size = 0.025, max.iter = 10, max.sentence.length = 1000,
+                         window.size = 1L, get.vectors = FALSE, ...) {
+  
   ml_backwards_compatibility_api()
+  df <- spark_dataframe(x)
+  sc <- spark_connection(df)
   class <- "org.apache.spark.ml.feature.Word2Vec"
-  invoke_simple_transformer(x, class, list(setInputCol = ensure_scalar_character(input.col),
-                                           setOutputCol = ensure_scalar_character(output.col),
-                                           setVectorSize = ensure_scalar_integer(vector.size),
-                                           setMinCount = ensure_scalar_integer(min.count),
-                                           setWindowSize = ensure_scalar_integer(window.size),
-                                           setNumPartitions = ensure_scalar_integer(num.partitions),
-                                           setStepSize = ensure_scalar_double(step.size),
-                                           setMaxIter = ensure_scalar_integer(max.iter),
-                                           function(transformer, sdf) invoke(transformer, "fit", sdf)))
+  
+  result <- invoke_new(sc, class) %>%
+    invoke("setInputCol", ensure_scalar_character(input.col)) %>%
+    invoke("setOutputCol", ensure_scalar_character(output.col)) %>%
+    invoke("setVectorSize", ensure_scalar_integer(vector.size)) %>%
+    invoke("setMinCount", ensure_scalar_integer(min.count)) %>%
+    invoke("setNumPartitions", ensure_scalar_integer(num.partitions)) %>%
+    invoke("setStepSize", ensure_scalar_double(step.size)) %>%
+    invoke("setMaxIter", ensure_scalar_integer(max.iter)) %>%
+    invoke("setMaxSentenceLength", ensure_scalar_integer(max.sentence.length)) %>%
+    invoke("fit", df)
+  
+  if(get.vectors){
+    result %>%
+      invoke("getVectors") %>%
+      sdf_register()
+  }else{
+    result %>%
+      invoke("transform") %>%
+      sdf_register()
+  }
 }
 environment(ft_word2Vec) <- asNamespace('sparklyr')

033ea60cd6dee27e5010b77d7cbb988fbb0f56ce
catlain <catlain@vip.qq.com>
2017-05-27 21:21:42 +0800

    ft_geteval

---
+++ R/ft_geteval.R
@@ -0,0 +1,12 @@
+ft_geteval <- function(fit, df, binary = TRUE) {
+  eval <- lapply(df, function(x){
+    
+    if(binary){
+      sdf_predict(fit, x) %>%
+        ml_binary_classification_eval("label", "prediction")
+    }else{
+      sdf_predict(fit, x) %>%
+        ml_classification_eval("label", "prediction", "accuracy")
+    }
+  })
+}

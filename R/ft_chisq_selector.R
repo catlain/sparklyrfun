58f65fd9edd4743d3224bda0bae2bdab633e82ba
catlain <catlain@vip.qq.com>
2017-06-21 17:48:20 +0800

    ft_chisq_selector

---
+++ R/ft_chisq_selector.R
@@ -0,0 +1,10 @@
+ft_chisq_selector <- function (x, features.col = "features", label.col = "label", output.col = "selected_features", num.top.features = 50, ...) {
+  ml_backwards_compatibility_api()
+  class <- "org.apache.spark.ml.feature.ChiSqSelector"
+  
+  invoke_simple_transformer(x, class, list(setFeaturesCol = ensure_scalar_character(features.col),
+                                           setLabelCol = ensure_scalar_character(label.col),
+                                           setOutputCol = ensure_scalar_character(output.col),
+                                           setNumTopFeatures = ensure_scalar_integer(num.top.features)))
+}
+environment(ft_chisq_selector) <- asNamespace('sparklyr')
---
+++ R/ft_vector_slicer.R
@@ -0,0 +1,16 @@
+ft_vector_slicer <- function (x, input.col = NULL, output.col = NULL, indices = list(1), ...) {
+  ml_backwards_compatibility_api()
+  class <- "org.apache.spark.ml.feature.VectorSlicer"
+  
+  if(is.list(indices)){
+    indices <- lapply(indices, function(x) x - 1)  # sacla array start from 0 !!
+  }else{
+    message(paste("indices need to be a list, but a", class(indices)))
+    indices <- list()
+  }
+  
+  invoke_simple_transformer(x, class, list(setInputCol = ensure_scalar_character(input.col),
+                                           setOutputCol = ensure_scalar_character(output.col),
+                                           setIndices = indices))
+}
+environment(ft_vector_slicer) <- asNamespace('sparklyr')

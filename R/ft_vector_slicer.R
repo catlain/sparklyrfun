62f38798c8251de362b2fdc00b3f66eaef496fc2
catlain <catlain@vip.qq.com>
2017-06-28 15:43:02 +0800

    vector_slicer as.array

--- R/ft_vector_slicer.R
+++ R/ft_vector_slicer.R
@@ -1,9 +1,9 @@
 ft_vector_slicer <- function (x, input.col = NULL, output.col = NULL, indices = list(1L), ...) {
   ml_backwards_compatibility_api()
   class <- "org.apache.spark.ml.feature.VectorSlicer"
-  
+  # indices need to be array !!!!!
   if(is.list(indices)){
-    indices <- lapply(indices, function(x) x - 1)  # sacla array start from 0 !!
+    indices <- as.array(lapply(indices, function(x) ensure_scalar_integer(x - 1))) # sacla array start from 0 !!
   }else{
     message(paste("indices need to be a list, but a", class(indices)))
     indices <- list()

3d4f1c896f229ef900654758a378b7bde5b996d2
xuyang79 <xuyang26@jd.com>
2018-01-16 21:14:16 +0800

    fixed TFIDF

--- R/ft_TFIDF.R
+++ R/ft_TFIDF.R
@@ -4,8 +4,8 @@ ft_TFIDF <- function (x, input.col = NULL, output.col = NULL, num.features = 100
   IDF <- "org.apache.spark.ml.feature.IDF"
   rawFeatures_col <- paste0("rawFeatures_", round(runif(1)*10000))
   invoke_simple_transformer(x, HashingTF, list(setInputCol = ensure_scalar_character(input.col),
-                                               setOutputCol = rawFeatures_col),
-                                               setNumFeatures = ensure_scalar_integer(num.features)) %>%
+                                               setOutputCol = rawFeatures_col,
+                                               setNumFeatures = ensure_scalar_integer(num.features))) %>%
     invoke_simple_transformer(IDF, list(setInputCol = rawFeatures_col,
                                         setOutputCol = ensure_scalar_character(output.col),
                                         setMinDocFreq = ensure_scalar_integer(min.freq),

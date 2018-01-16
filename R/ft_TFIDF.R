59bafa2d59a69ab1e23b12ca34f897c2a29e4f8d
xuyang79 <xuyang26@jd.com>
2018-01-16 20:46:43 +0800

    mutiple TFIDF

--- R/ft_TFIDF.R
+++ R/ft_TFIDF.R
@@ -2,11 +2,11 @@ ft_TFIDF <- function (x, input.col = NULL, output.col = NULL, num.features = 100
   ml_backwards_compatibility_api()
   HashingTF <- "org.apache.spark.ml.feature.HashingTF"
   IDF <- "org.apache.spark.ml.feature.IDF"
-
+  rawFeatures_col <- paste0("rawFeatures_", round(runif(1)*10000))
   invoke_simple_transformer(x, HashingTF, list(setInputCol = ensure_scalar_character(input.col),
-                                               setOutputCol = "rawFeatures",
-                                               setNumFeatures = ensure_scalar_integer(num.features))) %>%
-    invoke_simple_transformer(IDF, list(setInputCol = "rawFeatures",
+                                               setOutputCol = rawFeatures_col),
+                                               setNumFeatures = ensure_scalar_integer(num.features)) %>%
+    invoke_simple_transformer(IDF, list(setInputCol = rawFeatures_col,
                                         setOutputCol = ensure_scalar_character(output.col),
                                         setMinDocFreq = ensure_scalar_integer(min.freq),
                                         function(transformer, sdf) invoke(transformer, "fit", sdf)))
